59691e5fb248b6f2512dcff3d6d04562d5af28e4
Stuart Reid <stuart1334@gmail.com>
2022-02-10 19:11:40 +0000

    Implement write trait

--- Kernel/src/vga_buffer.rs
+++ Kernel/src/vga_buffer.rs
@@ -1,3 +1,5 @@
+use core::fmt;
+use core::fmt::Write;
 use volatile::Volatile;
 
 #[allow(dead_code)]
@@ -90,6 +92,13 @@ impl Writer {
     }
 }
 
+impl fmt::Write for Writer {
+  fn write_str(&mut self, s: &str) -> fmt::Result {
+    self.write_string(s);
+    Ok(())
+  }
+}
+
 pub fn print_something() {
     let mut writer = Writer {
         column_position: 0,
@@ -98,6 +107,6 @@ pub fn print_something() {
     };
 
     writer.write_byte(b'H');
-    writer.write_string("ello ");
-    writer.write_string("World!");
+    writer.write_string("ello! ");
+    write!(writer, "The numbers are {} and {}", 42, 1.0/3.0).unwrap();
 }
