c334dc67f498d8f992090aee80da5b0abe7dfb93
Stuart Reid <stuart1334@gmail.com>
2022-02-21 13:49:38 +0000

    Add stack overflow double fault test

---
+++ kernel/tests/stack_overflow.rs
@@ -0,0 +1,55 @@
+#![no_std]
+#![no_main]
+#![feature(abi_x86_interrupt)]
+
+use core::panic::PanicInfo;
+use lazy_static::lazy_static;
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use kernel::{serial_print, serial_println, exit_qemu, QemuExitCode};
+
+lazy_static! {
+  static ref TEST_IDT: InterruptDescriptorTable = {
+    let mut idt = InterruptDescriptorTable::new();
+    unsafe {
+      idt.double_fault
+        .set_handler_fn(test_double_fault_handler)
+        .set_stack_index(kernel::gdt::DOUBLE_FAULT_IST_INDEX);
+    }
+
+    idt
+  };
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+  serial_print!("stack_overflow::stack_overflow...\t");
+
+  kernel::gdt::init();
+  init_test_id();
+
+  // trigger a stack overflow
+  stack_overflow();
+
+  panic!("Exection continued after stack overflow");
+}
+
+fn init_test_id() {
+  TEST_IDT.load();
+}
+
+extern "x86-interrupt" fn test_double_fault_handler(_stack_frame: InterruptStackFrame, _error_code: u64) -> ! {
+  serial_println!("[ok]");
+  exit_qemu(QemuExitCode::Success);
+  loop {}
+}
+
+#[allow(unconditional_recursion)]
+fn stack_overflow() {
+  stack_overflow();
+  volatile::Volatile::new(0).read();  // Prevent tail recursion optimization
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    kernel::test_panic_handler(info)
+}
