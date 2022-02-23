a9588200a184e83b7794c708ce80c2e132fe87c1
Stuart Reid <stuart1334@gmail.com>
2022-02-23 10:46:09 +0000

    Format some files

--- kernel/tests/stack_overflow.rs
+++ kernel/tests/stack_overflow.rs
@@ -3,50 +3,53 @@
 #![feature(abi_x86_interrupt)]
 
 use core::panic::PanicInfo;
+use kernel::{exit_qemu, serial_print, serial_println, QemuExitCode};
 use lazy_static::lazy_static;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
-use kernel::{serial_print, serial_println, exit_qemu, QemuExitCode};
 
 lazy_static! {
-  static ref TEST_IDT: InterruptDescriptorTable = {
-    let mut idt = InterruptDescriptorTable::new();
-    unsafe {
-      idt.double_fault
-        .set_handler_fn(test_double_fault_handler)
-        .set_stack_index(kernel::gdt::DOUBLE_FAULT_IST_INDEX);
-    }
+    static ref TEST_IDT: InterruptDescriptorTable = {
+        let mut idt = InterruptDescriptorTable::new();
+        unsafe {
+            idt.double_fault
+                .set_handler_fn(test_double_fault_handler)
+                .set_stack_index(kernel::gdt::DOUBLE_FAULT_IST_INDEX);
+        }
 
-    idt
-  };
+        idt
+    };
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-  serial_print!("stack_overflow::stack_overflow...\t");
+    serial_print!("stack_overflow::stack_overflow...\t");
 
-  kernel::gdt::init();
-  init_test_id();
+    kernel::gdt::init();
+    init_test_id();
 
-  // trigger a stack overflow
-  stack_overflow();
+    // trigger a stack overflow
+    stack_overflow();
 
-  panic!("Exection continued after stack overflow");
+    panic!("Exection continued after stack overflow");
 }
 
 fn init_test_id() {
-  TEST_IDT.load();
+    TEST_IDT.load();
 }
 
-extern "x86-interrupt" fn test_double_fault_handler(_stack_frame: InterruptStackFrame, _error_code: u64) -> ! {
-  serial_println!("[ok]");
-  exit_qemu(QemuExitCode::Success);
-  loop {}
+extern "x86-interrupt" fn test_double_fault_handler(
+    _stack_frame: InterruptStackFrame,
+    _error_code: u64,
+) -> ! {
+    serial_println!("[ok]");
+    exit_qemu(QemuExitCode::Success);
+    loop {}
 }
 
 #[allow(unconditional_recursion)]
 fn stack_overflow() {
-  stack_overflow();
-  volatile::Volatile::new(0).read();  // Prevent tail recursion optimization
+    stack_overflow();
+    volatile::Volatile::new(0).read(); // Prevent tail recursion optimization
 }
 
 #[panic_handler]
